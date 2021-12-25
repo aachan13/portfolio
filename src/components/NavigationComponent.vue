59b2aac947217a7ddb2c5aea897e5867e7e21033
Fadhil <afadhilprtm@gmail.com>
2021-12-25 11:04:32 +0800

    navbar component

---
+++ src/components/NavigationComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <nav class="flex justify-center space-x-4">
+        <a href="/dashboard" class="font-me px-3 py-2 text-gray-700 rounded-lg hover:bg-gray-100 hover:text-gray-900">Home</a>
+        <a href="/skills" class="font-me px-3 py-2 text-gray-700 rounded-lg hover:bg-gray-100 hover:text-gray-900">Skills</a>
+        <a href="/projects" class="font-me px-3 py-2 text-gray-700 rounded-lg hover:bg-gray-100 hover:text-gray-900">Projects</a>
+        <a href="/contact" class="font-me px-3 py-2 text-gray-700 rounded-lg hover:bg-gray-100 hover:text-gray-900">Contact</a>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavigationComponent',
+}
+</script>
