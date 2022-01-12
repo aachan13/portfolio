696d29fe9050be3d5f8dcc81bfef0b8bf359f573
Fadhil <afadhilprtm@gmail.com>
2022-01-12 21:34:38 +0800

    basic components for container

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <navigation-component></navigation-component>
+  <div class="container mx-auto p-4">
 
-  <div class="container p-5">
+    <navigation-component></navigation-component>
     <main-component></main-component>
   </div>
 </template>
@@ -71,8 +71,6 @@ export default {
   font-family: 'Calibre';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/NavigationComponent.vue
+++ src/components/NavigationComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="flex justify-center space-x-4">
+    <nav class="flex justify-end space-x-4">
         <a href="/dashboard" class="font-me px-3 py-2 text-gray-700 rounded-lg hover:bg-gray-100 hover:text-gray-900">Home</a>
         <a href="/skills" class="font-me px-3 py-2 text-gray-700 rounded-lg hover:bg-gray-100 hover:text-gray-900">Skills</a>
         <a href="/projects" class="font-me px-3 py-2 text-gray-700 rounded-lg hover:bg-gray-100 hover:text-gray-900">Projects</a>
